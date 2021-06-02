86edb755aa26b76f62b37f0636aab15698b6fbb1
shanimergui <69587799+shanimergui@users.noreply.github.com>
2021-06-02 11:13:06 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -6,6 +6,9 @@ namespace pandemic{
   int & Board::operator[](City city){
     return diseasCubes[city];
   }
+   void Board::remove_cures(){
+
+   }
   ostream& operator<<(ostream& os,const Board& board){
     return os;
   }
